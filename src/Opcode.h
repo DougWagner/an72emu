273f15da90e499a02391fd0bd5badd467b20321e
Doug Wagner <digwagner@gmail.com>
2017-02-07 18:51:08 -0800

    minor changes

--- src/Opcode.h
+++ src/Opcode.h
@@ -5,7 +5,7 @@
 
 namespace an72
 {
-    enum op_addr_mode
+    enum class op_addr_mode : uint8_t
     {
         none,
         immediate,
@@ -18,12 +18,13 @@ namespace an72
         indirect_y
     };
 
-    enum op_instr
+    enum class op_instr : uint8_t
     {
         adc_i, and_i, asl_i, bcc_i, bcs_i, beq_i, bit_i, bmi_i, bne_i, bpl_i, brk_i, bvc_i, bvs_i, clc_i,
         cld_i, cli_i, clv_i, cmp_i, cpx_i, cpy_i, dec_i, dex_i, dey_i, eor_i, inc_i, inx_i, iny_i, jmp_i, 
         jsr_i, lda_i, ldx_i, ldy_i, lsr_i, nop_i, ora_i, pha_i, php_i, pla_i, plp_i, rol_i, ror_i, rti_i, 
-        rts_i, sbc_i, sec_i, sed_i, sei_i, sta_i, stx_i, sty_i, tax_i, tay_i, tsx_i, txa_i, txs_i, tya_i
+        rts_i, sbc_i, sec_i, sed_i, sei_i, sta_i, stx_i, sty_i, tax_i, tay_i, tsx_i, txa_i, txs_i, tya_i,
+        illegal_instr
     };
 
     class Opcode
